34ec5356da08217951f5f05355da527510de1a6a
Dody Gunawinata <dodyg@silverkeytech.com>
2019-06-28 18:13:41 +0200

    Experimental work - might delete later

---
+++ WorldDiscovery.Web/World.fs
@@ -0,0 +1,23 @@
+﻿module World
+
+type Customer = 
+    { First: string
+      Last: string
+      SSN: uint32
+      AccountNumber: uint32; }
+
+
+type Country =
+    {
+        Name : string
+    }
+
+type World =
+    {
+        Countries : List<Country>
+    }
+
+let x : World = {
+    Countries = [ { Name = "Shit"}]
+};
+
