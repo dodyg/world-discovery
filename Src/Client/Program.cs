88df172d8e6fef37ab8734457af234adf4306e0a
Dody Gunawinata <dodyg@silverkeytech.com>
2022-08-30 17:34:39 +0200

    Install MudBlazor and clean up other template items.

--- Src/Client/Program.cs
+++ Src/Client/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WorldDiscovery.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
