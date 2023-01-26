0a2bac4a843437d2a65281365eaae8df41f097c6
Dody Gunawinata <dodyg@silverkeytech.com>
2023-01-26 19:46:31 +0200

    Clean up and upgrade dependencies

--- Src/Server/Program.cs
+++ Src/Server/Program.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.ResponseCompression;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -17,7 +13,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,7 +22,6 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 
 app.MapRazorPages();
 app.MapControllers();
